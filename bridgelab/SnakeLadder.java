8d8d3143ea2b1397948f1752619db4c51df59d71
praveen <praveenpkbadiger@gmail.com>
2022-08-04 17:32:28 +0530

    Getting random number 1 to 6

--- bridgelab/SnakeLadder.java
+++ bridgelab/SnakeLadder.java
@@ -6,6 +6,9 @@ public class SnakeLadder
     {
         int start = 0;
         System.out.println("Starting position of player is: " + start);
+        int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println(roll);
+
 
 
     }
